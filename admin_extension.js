997ea3ff9fd027bf5a70fed85e15879579a350e4
Martin Gottweis <martin.gottweis@booking.com>
2017-10-28 13:19:57 +0200

    added configuration and test asset folder

--- admin_extension.js
+++
@@ -1,1 +0,0 @@
-console.log('quill is here')
--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('123')
+const enduro_quill = function () {}
+
+enduro_quill.prototype.brick_configuration = {
+	admin_js_inject: 'inject_quill_to_admin.js'
+	default_settings: {
+		theme: 'Snow',
+		apply_to_all_textareas: false,
+	}
+}
+
+module.exports = new enduro_quill()
