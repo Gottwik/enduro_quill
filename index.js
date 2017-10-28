44bd1129f58edf86d871c308922da231f2a349e5
Martin Gottweis <martin.gottweis@booking.com>
2017-10-28 13:21:45 +0200

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const enduro_quill = function () {}
 
 enduro_quill.prototype.brick_configuration = {
-	admin_js_inject: 'inject_quill_to_admin.js'
+	admin_js_inject: 'inject_quill_to_admin.js',
 	default_settings: {
 		theme: 'Snow',
 		apply_to_all_textareas: false,
