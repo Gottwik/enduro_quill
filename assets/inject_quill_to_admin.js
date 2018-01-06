6a52ce19d623a4b37e1e263d5e623969b8664495
Martin Gottweis <martin.gottweis@booking.com>
2018-01-07 00:52:57 +0100

    added passing js settings for bricks

--- assets/inject_quill_to_admin.js
+++ assets/inject_quill_to_admin.js
@@ -1,5 +1,6 @@
 // inject quill css into the admin interface
 $('head').append('<link href="https://cdn.quilljs.com/1.3.3/quill.snow.css" rel="stylesheet">')
+$('head').append('<link href="https://cdn.quilljs.com/1.3.3/quill.bubble.css" rel="stylesheet">')
 
 // * ———————————————————————————————————————————————————————— * //
 // *	quill directive
@@ -10,9 +11,7 @@ enduro_admin_app.compileProvider
 			link: function (scope, element, attr) {
 				$.getScript('https://cdn.quilljs.com/1.3.3/quill.js', function () {
 
-					var editor = new Quill(element[0], {
-						theme: 'snow'
-					})
+					var editor = new Quill(element[0], brick_admin_settings.enduro_quill)
 
 					editor.on('text-change', function(delta, oldDelta, source) {
 						scope.context[scope.terminatedkey] = editor.root.innerHTML
